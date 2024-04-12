2011f14b08a41e29bfd2f2b5ed8a57ffc9a3ffb5
Srishti Sharma <77691682+srishti-sr@users.noreply.github.com>
2024-04-12 13:49:11 +0530

    Update Icache.scala

--- Icache.scala
+++ Icache.scala
@@ -1,1 +1,16 @@
 
+val tag_array  = DescribedSRAM(
+    name = "mem space",
+    desc = "ICache ",
+    size = 1,
+    data = Vec(2, UInt(tagBits.W))
+  )
+val tag_arrays = Seq.tabulate(tagbits_indexing_create_blocks) {
+    i =>
+      DescribedSRAM(
+        name = s"Tag space${i}",
+        desc = "ICache tag space",
+        size = 1,
+        data = Vec(2, UInt(tagBits.W))
+      )
+  }
